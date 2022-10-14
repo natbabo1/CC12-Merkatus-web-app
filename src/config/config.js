a12469b968e4e642b4752f4e623044d8544aef57
natbabo1 <rutsarun.lim@gmail.com>
2022-10-14 11:37:50 +0700

    feat: add essential folder, config, and router

---
+++ src/config/config.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import { API_ENDPOINT_URL } from './env';
+
+axios.defaults.baseURL = API_ENDPOINT_URL;
+
+axios.interceptors.request.use(
+  (config) => {
+    //   const token = getAccessToken();
+    //   if (token) {
+    //     config.headers.Authorization = `Bearer ${token}`;
+    //   }
+    return config;
+  },
+  (err) => Promise.reject(err)
+);
+
+axios.interceptors.response.use(
+  (response) => response,
+  (err) => {
+    //   if (err.response.status === 401) {
+    //     removeAccessToken();
+    //     return window.location.replace('/');
+    //   }
+    return Promise.reject(err);
+  }
+);
+
+export default axios;
