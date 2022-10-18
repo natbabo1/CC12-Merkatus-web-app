d7ca042755453eceff9de090d65b55bbe8e68c55
natbabo1 <rutsarun.lim@gmail.com>
2022-10-18 15:18:08 +0700

    feat: change fontawes icon to icon in BuyingPage

--- src/features/buyerDashboard/ProductFilterMenu.js
+++ src/features/buyerDashboard/ProductFilterMenu.js
@@ -1,27 +1,45 @@
+import WalletIcon from '../../components/icon/WalletIcon';
+import BoxIcon from '../../components/icon/BoxIcon';
+import TruckIcon from '../../components/icon/TruckIcon';
+import StarIcon from '../../components/icon/StarIcon';
+import StoreIcon from '../../components/icon/StoreIcon';
+
 function ProductFilterMenu() {
   return (
     <>
       <div className="flex flex-col items-center justify-center text-white grow gap-3 h-40 rounded-xl bg-vivid-orange cursor-pointer hover:bg-vivid-orange/80">
         <h2 className="text-xl">ที่ต้องชำระ</h2>
-        <i className="fa-solid fa-wallet text-7xl"></i>
+        <div className="w-[4.5rem]">
+          <WalletIcon />
+        </div>
       </div>
       <div className="flex flex-col items-center justify-center text-white grow gap-3 h-40 rounded-xl bg-vivid-orange cursor-pointer hover:bg-vivid-orange/80">
         <h2 className="text-xl">ที่ต้องจัดส่ง</h2>
-        <i className="fa-solid fa-cube text-7xl"></i>
+        <div className="w-[4.5rem]">
+          <BoxIcon />
+        </div>
       </div>
       <div className="flex flex-col items-center justify-center text-white grow gap-3 h-40 rounded-xl bg-vivid-orange cursor-pointer hover:bg-vivid-orange/80">
         <h2 className="text-xl">ที่ต้องได้รับ</h2>
-        <i className="fa-solid fa-truck-fast text-7xl"></i>
+        <div className="relative w-full h-[4.5rem]">
+          <div className="absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 w-28">
+            <TruckIcon />
+          </div>
+        </div>
       </div>
       <div className="flex flex-col items-center justify-center text-white grow gap-3 h-40 rounded-xl bg-vivid-orange cursor-pointer hover:bg-vivid-orange/80">
         <h2 className="text-xl">ให้คะแนน</h2>
-        <div className="relative w-20 h-20 border-4 border-white rounded-full">
-          <i className="fa-solid fa-star text-4xl absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2"></i>
+        <div className="w-16 h-[4.5rem]">
+          <StarIcon />
         </div>
       </div>
       <div className="flex flex-col items-center justify-center text-white grow gap-3 h-40 rounded-xl bg-vivid-orange cursor-pointer hover:bg-vivid-orange/80">
         <h2 className="text-xl">ร้านค้าของฉัน</h2>
-        <i className="fa-solid fa-store text-7xl"></i>
+        <div className="relative w-full h-[4.5rem]">
+          <div className="absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 w-20">
+            <StoreIcon />
+          </div>
+        </div>
       </div>
     </>
   );
