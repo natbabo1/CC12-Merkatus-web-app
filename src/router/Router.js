a0c4ebf0bce52973ae32f99dac7a40d5583394ef
purrpoom10 <thanawat.coding@gmail.com>
2022-10-19 16:37:53 +0700

    feat:addproduct

--- src/router/Router.js
+++ src/router/Router.js
@@ -1,20 +1,22 @@
-import { Routes, Route } from "react-router-dom";
-import MainLayout from "../layouts/MainLayout";
-import HomePage from "../pages/HomePage";
-import SellingPage from "../pages/SellingPage";
-import BuyingPage from "../pages/BuyingPage";
-import ProductPage from "../pages/ProductPage";
-import AddressEditPage from "../pages/AddressEditPage";
+import { Routes, Route } from 'react-router-dom';
+import MainLayout from '../layouts/MainLayout';
+import HomePage from '../pages/HomePage';
+import SellingPage from '../pages/SellingPage';
+import BuyingPage from '../pages/BuyingPage';
+import ProductPage from '../pages/ProductPage';
+import AddressEditPage from '../pages/AddressEditPage';
+import AddProductPage from '../pages/AddProductPage';
 
 function Router() {
   return (
     <Routes>
-      <Route path="/" element={<MainLayout />}>
+      <Route path='/' element={<MainLayout />}>
         <Route index element={<HomePage />} />
-        <Route path="selling" element={<SellingPage />} />
-        <Route path="buying" element={<BuyingPage />} />
-        <Route path="product" element={<ProductPage />} />
-        <Route path="address" element={<AddressEditPage />} />
+        <Route path='selling' element={<SellingPage />} />
+        <Route path='buying' element={<BuyingPage />} />
+        <Route path='product' element={<ProductPage />} />
+        <Route path='addproduct' element={<AddProductPage />} />
+        <Route path='address' element={<AddressEditPage />} />
       </Route>
     </Routes>
   );
