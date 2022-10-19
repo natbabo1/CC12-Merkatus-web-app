8f4238b2354a8a09747a4ff3b2142ffa21c66d2b
natbabo1 <rutsarun.lim@gmail.com>
2022-10-19 23:49:20 +0700

    refact: correct code to  component part

--- src/features/cart/CartContainer.js
+++ src/features/cart/CartContainer.js
@@ -2,19 +2,23 @@ import SellerCartGroup from "./SellerCardGroup";
 
 function CartContainer() {
   return (
-    <>
-      <form className="w-full border border-red-400 min-h-screen">
-        <hr className="border border-mermaid-net" />
-        <div className="w-3/4 mx-auto">
-          <SellerCartGroup />
-        </div>
-        <hr className="border border-mermaid-net" />
-      </form>
-      <div className="flex flex-col">
-        <div></div>
-        <div></div>
+    <form className="w-full min-h-screen">
+      <div className="w-3/4 mx-auto">
+        <SellerCartGroup />
+        <SellerCartGroup />
       </div>
-    </>
+      <div className="flex justify-end items-center w-full bg-vivid-orange/40 px-10 py-4 rounded-lg">
+        <div className="flex items-center text-vivid-orange font-semibold mr-14">
+          ราคารวมทั้งหมด
+          <span className="flex items-center text-black text-xl ml-3">
+            890 บาท
+          </span>
+        </div>
+        <button className="bg-vivid-orange text-white py-2 px-14 rounded-xl">
+          ชำระเงิน
+        </button>
+      </div>
+    </form>
   );
 }
 
--- src/features/cart/SellerCardGroup.js
+++ src/features/cart/SellerCardGroup.js
@@ -2,7 +2,8 @@ import ProductHorizonCard from "../../components/ui/ProductHorizonCard";
 function SellerCartGroup() {
   return (
     <>
-      <div className="mb-14">
+      <hr className="bg-mermaid-net border border-mermaid-net" />
+      <div className="my-10">
         <label className="flex items-center gap-3 text-2xl mb-10">
           <input
             type="radio"
@@ -34,50 +35,6 @@ function SellerCartGroup() {
             <ProductHorizonCard />
           </div>
         </div>
-      </div>
-      <div className="mb-14">
-        <label className="flex items-center gap-3 text-2xl">
-          <input
-            type="radio"
-            name="seller"
-            className="w-6 h-6 border-vivid-orange checked:bg-vivid-orange focus:ring-vivid-orange"
-          />
-          Flodo
-        </label>
-        <div className="flex flex-col gap-4 ml-10">
-          <div className="flex items-center justify-start gap-10">
-            <input
-              type="checkbox"
-              className="border-vivid-orange checked:bg-vivid-orange focus:ring-vivid-orange"
-            />
-            <ProductHorizonCard />
-          </div>
-          <div className="flex items-center justify-start gap-10">
-            <input
-              type="checkbox"
-              className="border-vivid-orange checked:bg-vivid-orange focus:ring-vivid-orange"
-            />
-            <ProductHorizonCard />
-          </div>
-          <div className="flex items-center justify-start gap-10">
-            <input
-              type="checkbox"
-              className="border-vivid-orange checked:bg-vivid-orange focus:ring-vivid-orange"
-            />
-            <ProductHorizonCard />
-          </div>
-        </div>
-      </div>
-      <div className="flex justify-end items-center w-full bg-vivid-orange/40 px-10 py-4 rounded-lg">
-        <div className="flex items-center text-vivid-orange font-semibold mr-14">
-          ราคารวมทั้งหมด
-          <span className="flex items-center text-black text-xl ml-3">
-            890 บาท
-          </span>
-        </div>
-        <button className="bg-vivid-orange text-white py-2 px-14 rounded-xl">
-          ชำระเงิน
-        </button>
       </div>
     </>
   );
