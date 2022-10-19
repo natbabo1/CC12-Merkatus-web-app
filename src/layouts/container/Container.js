ac810918d5160a0211b1fe6aef36853146eec0c5
natbabo1 <rutsarun.lim@gmail.com>
2022-10-19 14:52:31 +0700

    fix: css dimension, refact card to card component

--- src/layouts/container/Container.js
+++ src/layouts/container/Container.js
@@ -1,5 +1,9 @@
 function Container({ children }) {
-  return <div className="max-w-7xl mx-auto min-h-screen mt-28">{children}</div>;
+  return (
+    <div className="min-w-[80rem] max-w-7xl mx-auto min-h-screen mt-28">
+      {children}
+    </div>
+  );
 }
 
 export default Container;
