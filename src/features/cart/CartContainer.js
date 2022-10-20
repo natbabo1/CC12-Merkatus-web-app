aea2ec1838f4167029dfb8d07b4b4d4126fd5975
purrpoom10 <thanawat.coding@gmail.com>
2022-10-20 13:37:18 +0700

    feat:checkoutpage

--- src/features/cart/CartContainer.js
+++ src/features/cart/CartContainer.js
@@ -1,20 +1,20 @@
-import SellerCartGroup from "./SellerCardGroup";
+import SellerCartGroup from './SellerCardGroup';
 
 function CartContainer() {
   return (
-    <form className="w-full min-h-screen">
-      <div className="w-3/4 mx-auto">
+    <form className='w-full min-h-screen'>
+      <div className='w-3/4 mx-auto'>
         <SellerCartGroup />
         <SellerCartGroup />
       </div>
-      <div className="flex justify-end items-center w-full bg-vivid-orange/40 px-10 py-4 rounded-lg">
-        <div className="flex items-center text-vivid-orange font-semibold mr-14">
+      <div className='flex justify-end items-center w-full bg-vivid-orange/40 px-10 py-4 rounded-lg'>
+        <div className='flex items-center text-vivid-orange font-semibold mr-14'>
           ราคารวมทั้งหมด
-          <span className="flex items-center text-black text-xl ml-3">
+          <span className='flex items-center text-black text-xl ml-3'>
             890 บาท
           </span>
         </div>
-        <button className="bg-vivid-orange text-white py-2 px-14 rounded-xl">
+        <button className='bg-vivid-orange text-white py-2 px-14 rounded-xl'>
           ชำระเงิน
         </button>
       </div>
--- src/features/cart/SellerCardGroup.js
+++ src/features/cart/SellerCardGroup.js
@@ -1,36 +1,36 @@
-import ProductHorizonCard from "../../components/ui/ProductHorizonCard";
+import ProductHorizonCard from '../../components/ui/ProductHorizonCard';
 function SellerCartGroup() {
   return (
     <>
-      <hr className="bg-mermaid-net border border-mermaid-net" />
-      <div className="my-10">
-        <label className="flex items-center gap-3 text-2xl mb-10">
+      <hr className='bg-mermaid-net border border-mermaid-net' />
+      <div className='my-10'>
+        <label className='flex items-center gap-3 text-2xl mb-10'>
           <input
-            type="radio"
-            name="seller"
-            className="w-6 h-6 border-vivid-orange checked:bg-vivid-orange focus:ring-vivid-orange"
+            type='radio'
+            name='seller'
+            className='w-6 h-6 border-vivid-orange checked:bg-vivid-orange focus:ring-vivid-orange'
           />
           Galadriel
         </label>
-        <div className="flex flex-col gap-4 ml-10">
-          <div className="flex items-center justify-start gap-10">
+        <div className='flex flex-col gap-4 ml-10'>
+          <div className='flex items-center justify-start gap-10'>
             <input
-              type="checkbox"
-              className="border-vivid-orange checked:bg-vivid-orange focus:ring-vivid-orange"
+              type='checkbox'
+              className='border-vivid-orange checked:bg-vivid-orange focus:ring-vivid-orange'
             />
             <ProductHorizonCard />
           </div>
-          <div className="flex items-center justify-start gap-10">
+          <div className='flex items-center justify-start gap-10'>
             <input
-              type="checkbox"
-              className="border-vivid-orange checked:bg-vivid-orange focus:ring-vivid-orange"
+              type='checkbox'
+              className='border-vivid-orange checked:bg-vivid-orange focus:ring-vivid-orange'
             />
             <ProductHorizonCard />
           </div>
-          <div className="flex items-center justify-start gap-10">
+          <div className='flex items-center justify-start gap-10'>
             <input
-              type="checkbox"
-              className="border-vivid-orange checked:bg-vivid-orange focus:ring-vivid-orange"
+              type='checkbox'
+              className='border-vivid-orange checked:bg-vivid-orange focus:ring-vivid-orange'
             />
             <ProductHorizonCard />
           </div>
