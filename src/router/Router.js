520b8432f2174ac6d46758c8b1a477e4b1b70e39
purrpoom10 <thanawat.coding@gmail.com>
2022-10-20 10:59:27 +0700

    initial feat

--- src/router/Router.js
+++ src/router/Router.js
@@ -1,28 +1,30 @@
-import { Routes, Route } from "react-router-dom";
-import MainLayout from "../layouts/MainLayout";
-import HomePage from "../pages/HomePage";
-import SellingPage from "../pages/SellingPage";
-import BuyingPage from "../pages/BuyingPage";
-import ProductPage from "../pages/ProductPage";
-import CategoryPage from "../pages/CategoryPage";
-import SellerProfilePage from "../pages/SellerProfilePage";
-import AddressEditPage from "../pages/AddressEditPage";
-import AddProductPage from "../pages/AddProductPage";
-import CartPage from "../pages/CartPage";
+import { Routes, Route } from 'react-router-dom';
+import MainLayout from '../layouts/MainLayout';
+import HomePage from '../pages/HomePage';
+import SellingPage from '../pages/SellingPage';
+import BuyingPage from '../pages/BuyingPage';
+import ProductPage from '../pages/ProductPage';
+import CategoryPage from '../pages/CategoryPage';
+import SellerProfilePage from '../pages/SellerProfilePage';
+import AddressEditPage from '../pages/AddressEditPage';
+import AddProductPage from '../pages/AddProductPage';
+import CartPage from '../pages/CartPage';
+import CheckoutPage from '../pages/CheckoutPage';
 
 function Router() {
   return (
     <Routes>
-      <Route path="/" element={<MainLayout />}>
+      <Route path='/' element={<MainLayout />}>
         <Route index element={<HomePage />} />
-        <Route path="selling" element={<SellingPage />} />
-        <Route path="buying" element={<BuyingPage />} />
-        <Route path="product" element={<ProductPage />} />
-        <Route path="addproduct" element={<AddProductPage />} />
-        <Route path="category" element={<CategoryPage />} />
-        <Route path="address" element={<AddressEditPage />} />
-        <Route path="profile" element={<SellerProfilePage />} />
-        <Route path="mycart" element={<CartPage />} />
+        <Route path='selling' element={<SellingPage />} />
+        <Route path='buying' element={<BuyingPage />} />
+        <Route path='product' element={<ProductPage />} />
+        <Route path='addproduct' element={<AddProductPage />} />
+        <Route path='category' element={<CategoryPage />} />
+        <Route path='address' element={<AddressEditPage />} />
+        <Route path='profile' element={<SellerProfilePage />} />
+        <Route path='mycart' element={<CartPage />} />
+        <Route path='checkout' element={<CheckoutPage />} />
       </Route>
     </Routes>
   );
