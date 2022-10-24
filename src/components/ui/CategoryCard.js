a35d15c9ca18f70aba61b48959bfa0ad291f22df
natbabo1 <rutsarun.lim@gmail.com>
2022-10-24 11:39:57 +0700

    fix: CategoryCard

--- src/components/ui/CategoryCard.js
+++ src/components/ui/CategoryCard.js
@@ -1,21 +1,21 @@
 import { useNavigate } from "react-router-dom";
 
-function CategoryCard({ src, category, categoryId }) {
+function CategoryCard({ category }) {
   const navigate = useNavigate();
   const handleClick = () => {
-    navigate(`/category/${categoryId}`);
+    navigate(`/category/${category.id}`);
   };
 
   return (
-    <div className="w-48" onClick={handleClick}>
+    <div className="w-48 h-64" onClick={handleClick}>
       <div className="relative w-full pt-[100%] overflow-hidden">
         <img
           className="block absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 w-full h-auto max-h-full max-w-full rounded-lg"
-          src={src}
+          src={category.image}
           alt="category"
         />
       </div>
-      <div className="mt-4">{category}</div>
+      <div className="mt-4">{category.categoryName}</div>
     </div>
   );
 }
